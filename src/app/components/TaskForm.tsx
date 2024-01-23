29a1ed09ec589fd72fd4a15aa5a56c838952e1e7
Devin Jaggernauth <neexis@gmail.com>
2024-01-22 21:29:02 -0400

    fix onchange handler

--- src/app/components/TaskForm.tsx
+++ src/app/components/TaskForm.tsx
@@ -4,16 +4,16 @@ import {
   Box,
   Button,
   FormControl,
-  FormLabel,
   InputLabel,
   MenuItem,
+  Paper,
   Select,
+  SelectChangeEvent,
   Stack,
   TextField,
   Typography,
 } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, FormEventHandler, useState } from 'react';
 
 type FormData = {
   title: string;
@@ -25,95 +25,128 @@ type FormData = {
   status: string;
 };
 
-const priorityLevels = ['Low', 'Medium', 'High'];
-const statusOptions = ['Pending', 'In Progress', 'Completed'];
+const defaultTaskFormVales = {
+  title: '',
+  description: '',
+  dueDate: '',
+  assignee: '',
+  priorityLevel: '',
+  notes: '',
+  status: '',
+};
 
 const TaskForm = () => {
-  const [formData, setFormData] = useState<FormData>({
-    title: '',
-    description: '',
-    dueDate: '',
-    assignee: '',
-    priorityLevel: '',
-    notes: '',
-    status: '',
-  });
+  const [formData, setFormData] = useState<FormData>(defaultTaskFormVales);
 
-  const handleChange = (field: keyof FormData, value: string) => {
-    setFormData((prevData) => ({ ...prevData, [field]: value }));
+  const handleInputChange = (
+    event:
+      | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      | SelectChangeEvent
+  ) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
     console.log(formData);
   };
 
   return (
-    <Box m="auto" maxWidth="320px">
-      <Typography variant="h5" component="h2" align="center" mb={4}>
-        New Task
-      </Typography>
-      <form>
-        <Stack spacing={3}>
-          <TextField id="title" label="Title" size="small" required />
-          <TextField
-            id="description"
-            label="Description"
-            size="small"
-            required
-          />
-          <TextField
-            id="dueDate"
-            size="small"
-            type="date"
-            label="Due Date"
-            InputLabelProps={{ shrink: true }}
-          />
-          <TextField id="assignee" label="Assignee" size="small" />
+    <Box m="auto" maxWidth="360px">
+      <Paper elevation={3} sx={{ p: 4 }}>
+        <Typography variant="h5" component="h2" align="center" mb={4}>
+          New Task
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={3}>
+            <TextField
+              id="title"
+              name="title"
+              label="Title"
+              size="small"
+              required
+              onChange={handleInputChange}
+              value={formData.title}
+            />
+            <TextField
+              id="description"
+              name='description'
+              label="Description"
+              size="small"
+              required
+              onChange={handleInputChange}
+              value={formData.description}
+            />
+            <TextField
+              id="dueDate"
+              name='dueDate'
+              size="small"
+              type="date"
+              label="Due Date"
+              InputLabelProps={{ shrink: true }}
+              onChange={handleInputChange}
+              value={formData.dueDate}
+            />
+            <TextField
+              id="assignee"
+              name='assignee'
+              label="Assignee"
+              size="small"
+              onChange={handleInputChange}
+              value={formData.assignee}
+            />
 
-          <FormControl size="small">
-            <InputLabel id="priority">Priority</InputLabel>
-            <Select
-              labelId="priority"
-              label="Priority"
-              id="priority"
-              onChange={() => {}}
-              // value
-            >
-              <MenuItem value="low">Low</MenuItem>
-              <MenuItem value="medium">Medium</MenuItem>
-              <MenuItem value="high">High</MenuItem>
-            </Select>
-          </FormControl>
+            <FormControl size="small">
+              <InputLabel id="priorityLevel">Priority</InputLabel>
+              <Select
+                labelId="priorityLevel"
+                label="Priority"
+                id="priorityLevel"
+                name='priorityLevel'
+                onChange={handleInputChange}
+                value={formData.priorityLevel}
+              >
+                <MenuItem value="low">Low</MenuItem>
+                <MenuItem value="medium">Medium</MenuItem>
+                <MenuItem value="high">High</MenuItem>
+              </Select>
+            </FormControl>
 
-          <TextField
-            id="notes"
-            label="Notes"
-            multiline
-            size="small"
-            minRows={3}
-          />
+            <TextField
+              id="notes"
+              name='notes'
+              label="Notes"
+              multiline
+              size="small"
+              minRows={3}
+              onChange={handleInputChange}
+              value={formData.notes}
+            />
 
-          <FormControl size="small">
-            <InputLabel id="status">Status</InputLabel>
-            <Select
-              labelId="status"
-              label="Status"
-              id="status"
-              onChange={() => {}}
-              // value
-            >
-              <MenuItem value="pending">Pending</MenuItem>
-              <MenuItem value="inProgress">In Progress</MenuItem>
-              <MenuItem value="completed">Completed</MenuItem>
-              <MenuItem value="cancelled">Cancelled</MenuItem>
-            </Select>
-          </FormControl>
+            <FormControl size="small">
+              <InputLabel id="status">Status</InputLabel>
+              <Select
+                labelId="status"
+                label="Status"
+                id="status"
+                name='status'
+                onChange={handleInputChange}
+                value={formData.status}
+              >
+                <MenuItem value="pending">Pending</MenuItem>
+                <MenuItem value="inProgress">In Progress</MenuItem>
+                <MenuItem value="completed">Completed</MenuItem>
+                <MenuItem value="cancelled">Cancelled</MenuItem>
+              </Select>
+            </FormControl>
 
-          <Button type="submit" variant="contained">
-            Submit
-          </Button>
-        </Stack>
-      </form>
+            <Button type="submit" variant="contained" size="large">
+              Submit
+            </Button>
+          </Stack>
+        </form>
+      </Paper>
     </Box>
   );
 };
